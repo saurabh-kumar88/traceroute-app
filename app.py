b076cace9bc1ea4aa2092f90e894413876d066b9
saurabh-kumar88 <ykings.saurabh@gmail.com>
2021-02-11 00:19:25 +0530

    implemented basic structure class

--- app.py
+++ app.py
@@ -1,23 +1,44 @@
+import subprocess
+import os
 from ip2geotools.databases.noncommercial import DbIpCity
+import time
+from console_progressbar import ProgressBar
 
 
-def test1(ip_add):
-    response = DbIpCity.get(ip_add, api_key='free')
-    return {'lat': response.latitude,
-            'log': response.longitude}
+class TraceRoute:
+    def __init__(self):
+        pass
 
+    def runCommand(self, cmd, args):
+        # run shell command in subprocess and save the resuts in array as strings
+        self.cmd = cmd
+        self.args = args
+        route_data = []
+        response = subprocess.run([cmd, args], stdout=subprocess.PIPE)
+        # print(response.stdout.decode('utf-8'))
+        route_data.append(response.stdout.decode('utf-8'))
+        return route_data
 
-ip_list = [
-    '59.180.212.202',
-    '59.180.210.150',
-    '72.14.208.12',
-    '172.253.68.91',
-    '172.253.67.99',
-    '216.58.196.100'
-]
+    def dataCleanig(self):
+        # extract ip addresses from shell command outputs
+        pass
+
+    def getCoordinates(self, ip_add):
+        # get latitudes and longitudes from routers ip addresses
+        self.ip_add = ip_add
+        response = DbIpCity.get(ip_add, api_key='free')
+        return {'lat': response.latitude,
+                'log': response.longitude}
+
+    def mapPlotter(self):
+        # plot coordinates on map
+        pass
 
 
 if __name__ == "__main__":
-    for ip in ip_list:
-        print(test1(ip))
-    # print(test1('72.14.208.12'))
+    obj = TraceRoute()
+    pb = ProgressBar(total=100, prefix='Here', suffix='Now',
+                     decimals=3, length=50, fill='X', zfill='-')
+    pb.print_progress_bar(2)
+    time.sleep(5)
+    print(obj.runCommand('ping', 'google.com'))

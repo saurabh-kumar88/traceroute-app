08894a2165b47a6dbf37c65be7c0f931b82864ff
saurabh-kumar88 <ykings.saurabh@gmail.com>
2021-02-13 21:52:35 +0530

    updated app.py

--- app.py
+++ app.py
@@ -14,14 +14,23 @@ class TraceRoute:
         self.cmd = cmd
         self.args = args
         route_data = []
+        subprocess.run([cmd, args])
         response = subprocess.run([cmd, args], stdout=subprocess.PIPE)
-        # print(response.stdout.decode('utf-8'))
-        route_data.append(response.stdout.decode('utf-8'))
-        return route_data
+        print(response.stdout.decode('utf-8'))
+        try:
+            with open('logs.txt', 'w+') as file:
+                file.write(response.stdout.decode('utf-8'))
+        except Exception as err:
+            return False
 
-    def dataCleanig(self):
+        return True
+
+    def dataCleanig_tracetroute(self):
         # extract ip addresses from shell command outputs
-        pass
+        self.shell_output = shell_output
+        print(' ------- cleaning this data --------- ')
+
+        return shell_output
 
     def getCoordinates(self, ip_add):
         # get latitudes and longitudes from routers ip addresses
@@ -37,8 +46,15 @@ class TraceRoute:
 
 if __name__ == "__main__":
     obj = TraceRoute()
-    pb = ProgressBar(total=100, prefix='Here', suffix='Now',
-                     decimals=3, length=50, fill='X', zfill='-')
-    pb.print_progress_bar(2)
-    time.sleep(5)
-    print(obj.runCommand('ping', 'google.com'))
+    # pb = ProgressBar(total=100, prefix='Here', suffix='Now',
+    #                  decimals=3, length=50, fill='X', zfill='-')
+    # pb.print_progress_bar(2)
+    # time.sleep(5)
+    # print(obj.runCommand('tracert', 'google.com'))
+
+    # data = obj.runCommand('tracert', 'google.com')
+    # print(obj.dataCleanig_tracetroute(data))
+    subprocess.Popen('dir', shell=True)
+
+    # stdout = out.stdout.decode('utf-8')
+    # print(stdout)
